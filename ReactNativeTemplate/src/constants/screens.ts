36a0ce8ebe458d3868eb0da459e6157fd9d82353
Toki Horii <toki.h@x-point-1.net>
2020-08-30 22:26:55 +0900

    typo戻し

--- ReactNativeTemplate/src/constants/screens.ts
+++ ReactNativeTemplate/src/constants/screens.ts
@@ -1,7 +1,7 @@
 import HomeScreen from '@src/screens/HomeScreen/screen.component'
 
 export const screens = {
-  home: {
+  Home: {
     routeName: 'Home',
     displayName: 'ホーム',
     component: HomeScreen,
